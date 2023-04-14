7be83d0218ab4e0b304f7787de8c6c34ee75d23f
ultor <ultorhash@gmail.com>
2023-04-14 21:35:54 +0200

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import {
+  RoutingModule,
+  LibraryModule
+} from './modules';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    RoutingModule,
+    LibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
