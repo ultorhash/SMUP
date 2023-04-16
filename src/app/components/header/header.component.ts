2812c828f238fd09e9cef2aace7634ad88154dea
ultor <ultorhash@gmail.com>
2023-04-16 10:37:09 +0200

    added theme toggle

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { ISubpage } from '../../interfaces';
+import {
+  Component,
+  Inject,
+  OnInit,
+  Renderer2
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { ISubpage } from '../../interfaces';
+import { ThemeClasses, Themes } from '../../enums';
+import { PreferenceService } from '../../services';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   public isChecked: boolean = false;
   public subpages: ISubpage[] = [
     {
@@ -20,7 +28,29 @@ export class HeaderComponent {
     }
   ];
 
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+    private preferenceService: PreferenceService
+  ) {}
+
+  ngOnInit(): void {
+    const theme = this.preferenceService.getTheme();
+    this.isChecked = theme === Themes.DARK;
+    const themeClass = this.isChecked ? ThemeClasses.DARK_CLASS : ThemeClasses.LIGHT_CLASS;
+
+    this.setAppTheme(themeClass);
+  }
+
   onThemeChange({ checked }: MatSlideToggleChange): void {
+    const themeClass = checked ? ThemeClasses.DARK_CLASS : ThemeClasses.LIGHT_CLASS;
+  
     this.isChecked = checked;
+    this.preferenceService.saveTheme(checked ? Themes.DARK : Themes.LIGHT);
+    this.setAppTheme(themeClass);
+  }
+
+  private setAppTheme(themeClass: ThemeClasses): void {
+    this.renderer.setAttribute(this.document.body, 'class', themeClass);
   }
 }
